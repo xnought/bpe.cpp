5e6b9829c57977cc2923c35a295745299e565d22
xnought <drbertucci@hotmail.com>
2025-03-22 00:27:46 -0400

    add sorted

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # lightning-bpe
 
 Fast BPE training
+
+- [ ] Iterate given merge vocabs
+- [ ] Add string to vector sorted by length  
--- bpe.cpp
+++ bpe.cpp
@@ -29,7 +29,7 @@ bool starts_with(size_t a_offset, string &a, string &b)
 
 size_t find_slice_match(size_t i, string &s, vector<string> &new_vocab)
 {
-	for (size_t j = 0; j < new_vocab.size(); j++)
+	for (int j = new_vocab.size() - 1; j >= 0; j--)
 	{
 		string v = new_vocab[j];
 		bool fits = (i + v.size()) < s.size();
@@ -60,10 +60,26 @@ string substr_between(string &s, size_t a, size_t b)
 	return s.substr(a, b - a);
 }
 
+void insert_sorted_str_size(vector<string> &v, string &s)
+{
+	for (int i = v.size() - 1; i >= 0; i--)
+	{
+		string item = v[i];
+		if (s.size() >= item.size())
+		{
+			v.insert(v.begin() + i + 1, s);
+			return;
+		}
+	}
+}
+
 int main()
 {
 	string s[1] = {"hi there my name is donny"};
-	vector<string> vocab = {"hi t", "hi ", "hi", "do"};
+	vector<string> vocab = {"do", "hi", "hi ", "hi t"};
+	string a = "hi there";
+	insert_sorted_str_size(vocab, a);
+
 	vector<size_t> idxs = token_idxs(s[0], vocab);
 
 	for (size_t i = 0; i < idxs.size() - 1; i++)
